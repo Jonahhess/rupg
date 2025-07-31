76832ab2a0bb27ac0782a594b4f271d7c296d12b
jonahhess <jonahhessdev@gmail.com>
2025-07-31 10:36:17 +0300

    added api keys to production

--- main.js
+++ main.js
@@ -10,7 +10,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // add api_name: key pairs to mocker
     const randomUserGenerator = "random_user_generator";
-    mocker.add(randomUserGenerator, apiKeys.get(randomUserGenerator));
+    mocker.add(randomUserGenerator, apiKeys.randomUserGenerator);
+
+    const randomKanyeQuote = "random_kanye_quote";
+    mocker.add(randomKanyeQuote, apiKeys.randomKanyeQuote);
+
+    const pokeAPI = "poke_api";
+    mocker.add(pokeAPI.apiKeys.pokeAPI);
+
+    const baconIpsum = "bacon_ipsum";
+    mocker.add(baconIpsum, apiKeys.baconIpsum);
 
     // API 2:
 
