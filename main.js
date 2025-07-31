a096eed4f070a986640a60c85b305b1ef579738b
jonahhess <jonahhessdev@gmail.com>
2025-07-31 12:05:32 +0300

    using getData instead of mocker

--- main.js
+++ main.js
@@ -1,38 +1,27 @@
-import Mocker from "./mocker.js";
+import getData from "./getData.js";
 import apiKeys from "./apiKeys.js";
 import parser from "./parser.js";
 document.addEventListener("DOMContentLoaded", () => {
   const generateUserButton = document.getElementById("generate-user");
   generateUserButton.addEventListener("click", async () => {
-    const mocker = new Mocker();
-
-    // add api_name: key pairs to mocker
-    const randomUserGenerator = "random_user_generator";
-    mocker.add(randomUserGenerator, apiKeys.randomUserGenerator);
-
-    const randomKanyeQuote = "random_kanye_quote";
-    mocker.add(randomKanyeQuote, apiKeys.randomKanyeQuote);
-
-    const pokeAPI = "poke_api";
-    mocker.add(pokeAPI.apiKeys.pokeAPI);
-
-    const baconIpsum = "bacon_ipsum";
-    mocker.add(baconIpsum, apiKeys.baconIpsum);
-
-    const userPromise = mocker.getData(
-      randomUserGenerator,
+    const userPromise = getData(
       "www.random.com",
+      apiKeys.randomUserGenerator,
       "limit=7"
     );
 
     const kanyePromise = mocker.getData(
-      randomKanyeQuote,
       "www.yeezy.com",
+      apiKeys.randomKanyeQuote,
       "limit=1"
     );
 
-    const pokePromise = mocker.getData(pokeAPI, "www.pokedex.org", "limit=1");
-    const baconPromise = mocker.getData(baconIpsum, "www.bacon.com");
+    const pokePromise = mocker.getData(
+      "www.pokedex.org",
+      apiKeys.pokeAPI,
+      "limit=1"
+    );
+    const baconPromise = mocker.getData("www.bacon.com", apiKeys.baconIpsum);
 
     const [users, kanyeText, pokeInfo, baconText] = await Promise.all([
       userPromise,
