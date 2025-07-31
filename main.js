f62f806c06094ed84b0ca835f4438951a2138620
jonahhess <jonahhessdev@gmail.com>
2025-07-31 13:29:51 +0300

    made code shorter, more readable

--- main.js
+++ main.js
@@ -1,38 +1,21 @@
 import getData from "./getData.js";
-import apiKeys from "./apiKeys.js";
-import parser from "./parser.js";
+import parse from "./parser.js";
 document.addEventListener("DOMContentLoaded", () => {
   const generateUserButton = document.getElementById("generate-user");
   generateUserButton.addEventListener("click", async () => {
-    const userPromise = getData(
-      "www.random.com",
-      apiKeys.randomUserGenerator,
-      "limit=7"
-    );
-
-    const kanyePromise = mocker.getData(
-      "www.yeezy.com",
-      apiKeys.randomKanyeQuote,
-      "limit=1"
-    );
-
-    const pokePromise = mocker.getData(
-      "www.pokedex.org",
-      apiKeys.pokeAPI,
-      "limit=1"
-    );
-    const baconPromise = mocker.getData("www.bacon.com", apiKeys.baconIpsum);
-
-    const [users, kanyeText, pokeInfo, baconText] = await Promise.all([
-      userPromise,
-      kanyePromise,
-      pokePromise,
-      baconPromise,
+    const [users, kanye, pokedex, baconIpsum] = await Promise.all([
+      getData("https://randomuser.me/api/?results=7"),
+      getData("https://api.kanye.rest"),
+      getData("https://pokeapi.co/api/v2/pokemon/3"),
+      getData("https://baconipsum.com/api/?type=meat-and-filler"),
     ]);
 
+    // prepare data for page
+    const parser = parse();
     const [currentUser, ...friends] = parser.parseUsers(users);
-    const kanyeQuote = `'${kanyeText}' - Kanye West`;
-    const pokemon = parser.parsePokemon(pokeInfo);
+    const kanyeQuote = `'${kanye.quote}' - Kanye West`;
+    const pokemon = parser.parsePokemon(pokedex);
+    const bacon = baconIpsum.join("\n");
 
     // Attach info to page
     const div = document.createElement("div");
