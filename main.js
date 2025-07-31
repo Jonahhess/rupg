10e68a853545e6acd8987ef491fd54a699488bb1
jonahhess <jonahhessdev@gmail.com>
2025-07-31 09:55:18 +0300

    added button listener

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import Mock from "./mock.js";
 document.addEventListener("DOMContentLoaded", () => {
   console.log("hello world");
+
+  const generateUserButton = document.getElementById("generate-user");
+  generateUserButton.addEventListener("click", () => {
+    console.log("hello world!");
+  });
 });
