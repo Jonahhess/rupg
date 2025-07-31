05562cebcb4f672f050cc0258dde8bce89c72f16
jonahhess <jonahhessdev@gmail.com>
2025-07-31 10:21:44 +0300

    added basic functionality

--- mock.js
+++ mock.js
@@ -1,9 +1,31 @@
 export default class Mock {
-  #source;
-  #api_key;
-  constructor(source, api_key, url) {
-    this.#source = source;
-    this.#api_key = api_key;
-    localStorage.setItem(source, api_key);
+  add(api_name, api_key) {
+    localStorage.setItem(api_name, api_key);
+  }
+
+  remove(key) {
+    localStorage.removeItem(key);
+  }
+
+  async getData(api_name, url, textAfterKey = "") {
+    const api_key = localStorage.getItem(api_name);
+    if (!api_key) throw new Error("No API Key");
+
+    const fullpath = `${url}?api_key=${api_key}${
+      textAfterKey ? `&${textAfterKey}` : ""
+    }`;
+
+    const cached = localStorage.getItem(fullpath);
+    if (cached) return JSON.parse(cached);
+
+    const response = await fetch(fullpath);
+    if (!response.ok) throw new Error("bad path");
+
+    const data = await response.json();
+
+    const stringifiedData = JSON.stringify(data);
+    localStorage.setItem(fullpath, stringifiedData);
+
+    return data;
   }
 }
