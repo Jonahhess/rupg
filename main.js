9ad821de51d79e123fba415cfa4a99b823239c88
jonahhess <jonahhessdev@gmail.com>
2025-07-31 10:23:45 +0300

    import apiKeys

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 import Mock from "./mock.js";
+import apiKeys from "./apiKeys.js";
 document.addEventListener("DOMContentLoaded", () => {
   console.log("hello world");
 
   const generateUserButton = document.getElementById("generate-user");
   generateUserButton.addEventListener("click", () => {
     console.log("hello world!");
+    const mocker = new Mock();
+
+    // add api_name: key pairs to mocker
+    mocker.add("random_user_generator");
+
+    // API 2:
+
+    // API 3:
+
+    // API 4:
   });
 });
