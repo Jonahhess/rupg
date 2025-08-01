a86b78463fb18de6f4e4433c48131e8a8ba6ae66
jonahhess <jonahhessdev@gmail.com>
2025-08-01 14:19:45 +0300

    added load, save user code, helper fns

--- update.js
+++ update.js
@@ -18,18 +18,72 @@ export async function init() {
   const pokemon = parser.parsePokemon(pokedex);
   const bacon = baconIpsum.join("\n");
 
-  return update([
-    ["currentUser", currentUser],
-    ["kanyeQuote", kanyeQuote],
-    ["pokemon", pokemon],
-    ["bacon", bacon],
-    ["friends", friends],
-  ]);
+  const user = {
+    currentUser,
+    kanyeQuote,
+    pokemon,
+    bacon,
+    friends,
+  };
+
+  for (const key in user) {
+    model[key] = user[key];
+  }
+
+  return model;
 }
 
-export function update(entries) {
-  for (const [key, value] of entries) {
-    model[key] = value;
+function assignUser(from, to = model) {
+  for (const key in model) {
+    to[key] = from[key];
   }
-  return model;
+
+  return to;
+}
+
+function validateShape(newObj, oldObj) {
+  for (const key in oldObj) {
+    const oldValue = oldObj[key];
+    const newValue = newObj[key];
+
+    // case 1: new obj missing value
+    if (!newValue) throw new Error(`missing value for: ${key}`);
+
+    // case 2: value is different type than original value
+    if (typeof oldValue !== typeof newValue) throw new Error("type mismatch");
+
+    // case 3: value is object - note:
+    if (typeof newValue === "object") {
+      validateShape(newValue, oldValue);
+    }
+  }
+}
+
+export function saveUser(user = model) {
+  if (typeof user !== "object") throw new Error("cannot save user");
+
+  const name = user.currentUser?.name;
+  if (!name || typeof name !== "string") throw new Error("invalid name");
+
+  const cache = localStorage.getItem(name);
+  if (cache) return;
+
+  const userToSave = assignUser(user, {});
+  validateShape(userToSave, model);
+  localStorage.setItem(
+    JSON.stringify(userToSave.currentUser.name), // note: names cannot collide
+    JSON.stringify(userToSave)
+  );
+}
+
+export function loadUser(name) {
+  if (typeof name !== "string") throw new Error("invalid name");
+  const cache = localStorage.getItem(name);
+  if (!cache) throw new Error(`User '${name}' not found`);
+  return JSON.parse(cache);
+}
+
+export function update(user) {
+  validateShape(user);
+  return assignUser(user);
 }
